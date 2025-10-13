8f52533a77087489a13eb695fe09619c5aad28d0
LakshP77 <lspvsp@gmail.com>
2025-10-13 23:14:48 +0000

    yuryeiuh

---
+++ nestedIfStatments.java
@@ -0,0 +1,36 @@
+/*
+public class nestedIfStatments {
+    public static void main(String[] args) {
+
+        boolean isStudent = false;
+        boolean isSenior = false;
+
+        double price = 9.99;
+        
+        if(isStudent) {
+            if(isSenior) {
+                System.out.println(" You get senior discount of 20% off! ");
+                System.out.println(" You get student discount of 10% off! ");
+                price = price * 0.7;
+            
+            } else {
+                System.out.println(" You get student discount of 10% off! ");
+                price = price * 0.9;
+            }
+
+        } else {
+            if(isSenior) {
+            System.out.println(" You get senior discount of 20% off! ");
+            price = price * 0.8;
+            }
+
+            else {
+                price = price;
+            }
+        }
+
+        System.out.printf("The price of the ticket is $%.2f " , price);
+    }
+}
+
+*/
